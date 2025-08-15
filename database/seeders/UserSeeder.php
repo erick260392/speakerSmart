83cce4113d1490dc83965eade88082140b34ea7c
erick260392 <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>
2025-08-14 22:14:39 -0600

    seedersDataBase

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@speakersmarter.com',
+            'password' => Hash::make('Admin123'),
+        ]);
+        $admin->assignRole('admin');
+        $editor = User::create([
+            'name' => 'Editor',
+            'email' => 'editor@speakersmarter.com',
+            'password' => Hash::make('Editor123'),
+        ]);
+        $editor->assignRole('editor');
+    }
+}
